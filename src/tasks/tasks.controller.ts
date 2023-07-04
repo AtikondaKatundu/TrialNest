2c84bbcf5a9c7479bf06e0cbc957dbd0181e1616
Atikonda Katundu <atikondakatundu@gmail.com>
2023-07-04 16:14:00 +0200

    Added get task by id method to service and controller

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task } from './task.model';
 import { CreateTaskDto } from './dtos/create-task.dto';
@@ -17,4 +17,8 @@ export class TasksController {
        return this.tasksService.createTask(createTaskDto); 
     }
 
+    @Get('/:id')
+    getTaskById(@Param('id') id: string) : Task{
+        return this.tasksService.getTaskById(id);
+    }
 }
